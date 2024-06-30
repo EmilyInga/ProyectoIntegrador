e59915de70fe7c169e257f16baa17ba8fd296e87
EmilyInga <157333437+EmilyInga@users.noreply.github.com>
2024-06-30 17:30:45 -0500

    SISTEMA DE INVENTARIOS TIENDA MAQUILLAJE.c

--- SISTEMA DE INVENTARIOS TIENDA MAQUILLAJE.c
+++ SISTEMA DE INVENTARIOS TIENDA MAQUILLAJE.c
@@ -1,17 +1,20 @@
 #include <stdio.h>
 #include <string.h>
+//Incluimos los headers
 #include "agregar.h"
 #include "editar.h"
 #include "enlistar.h"
 #include "eliminar.h"
+//declarar e inicializar en 0 veces
 int veces =1;
 
 int main(){
     int opcion, irMenu;
     printf("\n\n");
-    printf("\t\t INVENTARIO TIENDA MAKE UP \t\n");
+    printf("\t\t INVENTARIO Cosmética saludable \t\n");
     do{
         do{
+            //imprime el menu
             printf("\n");
             printf(" Actividades\t\n");
             printf(" 1. Agregar  productos \n");
@@ -20,12 +23,15 @@ int main(){
             printf(" 4. Enlistar productos \n");
             printf(" Que actividad vas a realizar: ");
             scanf("%d",&opcion);
-            
+            //controla que entre 1-4
         }while(opcion<0 || opcion>4);
         
+        //dirige al header
         switch(opcion){
         case 1:
         agregar();
+        // este veces va a marcar las veces que ingresa a agregar productos
+        // para que el encabezado se imprima solo la primera vez
         veces ++;
         break;
         case 2:
@@ -38,8 +44,11 @@ int main(){
         enlistar();
         break;
         }
+        // permite que realice mas actividades
         printf("Desea realizar otra actividad si(1) o no (2):");
         scanf ("%d",&irMenu);
+        
+        //controla que si es 2 sale del programa
     } while(irMenu==1);
 
     return 0;
